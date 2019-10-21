a94d7cade2314c8bc03441c1fbe8b6bc2091f802
Maksim Shatokhin <max.sh99@mail.ru>
2019-10-21 03:08:43 +0300

    update tests

--- lab_1/test.py
+++ lab_1/test.py
@@ -1,26 +1,34 @@
 #юнит тестирование
 
 from len_lev import lev_table
+from len_lev import dam_lev_table
+from len_lev import dam_lev_rec
 
 def print_table_header():
     print('|string 1', '|', 'string 2', '|',
-          'incorrect result', '|', 'correct result |') 
+          'incorrect result', '|', 'correct result', '|', 'passed', '|') 
 
 def print_table_line(str1, str2, res, correct_res):
+    passed = 'yes' if res == correct_res else 'NO!'
     print('|', str1.center(7), '|', str2.center(8), '|',
-              str(res).center(16), '|', str(correct_res).center(14), '|')
+              str(res).center(16), '|', str(correct_res).center(14), '|',
+              passed.center(6), '|')
 
 def test(str1, str2, correct_res, func):
-    res = func(str1, str2, False)
+    try:
+        res = func(str1, str2)
+    except:
+        res = 'ERROR'
+    print_table_line(str1, str2, res, correct_res)
     if res == correct_res:
         return 1
     else:
-        print_table_line(str1, str2, res, correct_res)
         return 0
     
 
-def check_function(func):
-    f = open('tests.txt', 'r')
+def check_function(func, testfile):
+    print('Текстируемая функция: ', str(testfile)[6:-4])
+    f = open(testfile, 'r')
     test_n = 0
     correct = 0
 
@@ -33,8 +41,10 @@ def check_function(func):
             correct += test(args[0], args[1], int(args[2]), func)
 
     if test_n == correct:
-        print('\nТестирование пройдено успешно,', correct, 'тестов из', test_n)
+        print('\nТестирование успешно,', correct, 'тестов из', test_n, '\n')
     else:
-        print('\nВыполнено верно тестов', correct, 'из', test_n)
+        print('\nВыполнено верно тестов', correct, 'из', test_n, '\n')
 
-check_function(lev_table)
+check_function(lev_table, 'tests_lev.txt')
+check_function(dam_lev_table, 'tests_dam_lev.txt')
+check_function(dam_lev_rec, 'tests_dam_lev.txt')
